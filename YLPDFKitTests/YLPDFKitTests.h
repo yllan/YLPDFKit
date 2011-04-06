01ab6c9821dd555b49c0552667412a6ea0c588c5
yllan <yllan@me.com>
2011-04-06 22:36:07 +0800

    Initial Commit

---
+++ YLPDFKitTests/YLPDFKitTests.h
@@ -0,0 +1,17 @@
+//
+//  YLPDFKitTests.h
+//  YLPDFKitTests
+//
+//  Created by Yung-Luen Lan on 4/6/11.
+//  Copyright 2011 yllan.org. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface YLPDFKitTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ YLPDFKitTests/YLPDFKitTests.m
@@ -0,0 +1,33 @@
+//
+//  YLPDFKitTests.m
+//  YLPDFKitTests
+//
+//  Created by Yung-Luen Lan on 4/6/11.
+//  Copyright 2011 yllan.org. All rights reserved.
+//
+
+#import "YLPDFKitTests.h"
+
+
+@implementation YLPDFKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in YLPDFKitTests");
+}
+
+@end
